1ea44820c5b1d08d649976b821cb4c4bf2b2d91f
vigrond <Vigrond@users.noreply.github.com>
2018-03-23 13:15:16 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -21,7 +21,7 @@ REST_FRAMEWORK = {
 SWAGGER_SETTINGS = {
     'USE_SESSION_AUTH': False,
     'SECURITY_DEFINITIONS': {
-        "VirtualAudience Server API - Swagger": {
+        "Your App API - Swagger": {
             "type": "oauth2",
             'authorizationUrl': '/yourapp/o/authorize',
             'tokenUrl': '/yourapp/o/token/',
